d7b924b44ee0911d2c237c42b22b0e87813f36a1
phuongdt606 <phuongdt606@gmail.com>
2016-05-19 11:45:04 +0700

    fix syle,  change icons

--- front/views/user/menu.php
+++ front/views/user/menu.php
@@ -4,7 +4,7 @@
   <div class="box-body">
   <ul>
     <li><i class="fa fa-info-circle"></i> <a href="/{$app->lang('common-url-account_info')}">{$app->lang('common-menu-account_info')}</a></li>
-    <li><i class="fa fa-info-circle"></i> <a href="/{$app->lang('common-url-change_password') }">{$app->lang('common-menu-change_password') }</a></li>
+    <li><i class="fa fa-key"></i> <a href="/{$app->lang('common-url-change_password') }">{$app->lang('common-menu-change_password') }</a></li>
     <li><i class="fa fa fa-sign-out"></i> <a href="/{$app->lang('common-url-logout') }">{$app->lang('common-menu-logout') }</a></li>
   </ul>
   </div>
