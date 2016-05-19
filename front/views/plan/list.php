dff5a2fe9f771734e4358c4fe1a9169244792062
dohaibac <dohaibac@gmail.com>
2016-05-19 10:47:30 +0700

    Update menu name to Lich Cong Tac

--- front/views/plan/list.php
+++ front/views/plan/list.php
@@ -1,6 +1,6 @@
 <div class="dashboard-main">
   <div class="col-lg-12 header-title">
-    <h1><i class="fa fa-th"></i> Danh sách Kế hoạch</h1>
+    <h1><i class="fa fa-th"></i> Lịch Công Tác</h1>
   </div>
    <div class="table-responsive">
       Đang cập nhật chức năng!
