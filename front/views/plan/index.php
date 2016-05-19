02b351c0e681e37fe060bbf73d5f67e9203092c1
dohaibac <dohaibac@gmail.com>
2016-05-19 15:39:07 +0700

    Add todoList JS for plan

--- front/views/plan/index.php
+++ front/views/plan/index.php
@@ -1,3 +1,8 @@
+<link href="{$app->appConf->theme_default}/css/font-awesome.min.css" rel="stylesheet">
+<link href="{$app->appConf->theme_default}/css/todo_list.css" rel="stylesheet">
+<link href="{$app->appConf->theme_default}/css/sticky.css" rel="stylesheet">
+
+<script type="text/javascript" src="{$app->appConf->theme_default}/js/controllers/plan.js"></script>
 <script type="text/javascript" src="{$app->appConf->theme_default}/js/controllers/group.js"></script>
 <script type="text/javascript" src="{$app->appConf->theme_default}/js/router/group.js"></script>
 
