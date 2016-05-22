32d7a7ebc3db19852c97294b1fcd4455412f487a
phuongdt606 <phuongdt606@gmail.com>
2016-05-23 01:42:44 +0700

    Short GD form

--- front/views/gd/add.php
+++ front/views/gd/add.php
@@ -17,16 +17,6 @@
       </div>
       <div class="clearfix"></div>
     </div>
-    <div class="form-group">
-      <div class="col-md-4 align-right">
-      <label class="control-label">Mã GET</label>
-      </div>
-      <div class="col-md-8">
-      <input ng-model="gd.code" type="input" name="code" class="form-control width100p" 
-          required autofocus ng-maxlength="255" placeholder="">
-       </div>
-       <div class="clearfix"></div>
-    </div>
     {literal}
     <div class="form-group1">
       <div class="col-md-4 align-right">
@@ -34,10 +24,10 @@
       </div>
       <div class="col-md-8">
          <ui-select ng-model="gd.sponsor" theme="bootstrap" class="width100p fleft">
-          <ui-select-match placeholder="Select or search a sponsor in the list...">{{$select.selected.username}}</ui-select-match>
+          <ui-select-match placeholder="Select or search a sponsor in the list...">{{$select.selected.sponsor}}</ui-select-match>
           <ui-select-choices repeat="item in sponsors | filter: $select.search">
-          <div ng-bind-html="item.name | highlight: $select.search"></div>
-          <small ng-bind-html="item.username | highlight: $select.search"></small>
+          <div ng-bind-html="item.sponsor | highlight: $select.search"></div>
+          <small ng-bind-html="item.sponsor | highlight: $select.search"></small>
           </ui-select-choices>
           </ui-select>
          <div class="clearfix"></div>
@@ -70,55 +60,6 @@
       <div class="col-md-8">
         <input type="radio" ng-model="gd.wallet" name="wallet" value="C-Wallet"> C-Wallet 
         <input type="radio" ng-model="gd.wallet" name="wallet" value="R-Wallet"> R-Wallet 
-      </div>
-      <div class="clearfix"></div>
-    </div>
-    {literal}
-    <div class="form-group1">
-      <div class="col-md-4 align-right">
-      <label class="control-label">Ngân hàng dự định GET</label>
-      </div>
-      <div class="col-md-8">
-         <ui-select ng-model="gd.bank" theme="bootstrap" class="width80p fleft">
-          <ui-select-match placeholder="Select or search a bank in the list...">{{$select.selected.name}} {{$select.selected.account_hold_name}} {{$select.selected.account_number}}</ui-select-match>
-          <ui-select-choices repeat="item in banks | filter: $select.search">
-          <div ng-bind-html="item.name | highlight: $select.search"></div>
-          <small ng-bind-html="item.account_hold_name + ' ' + item.account_number | highlight: $select.search"></small>
-          </ui-select-choices>
-     </ui-select> &nbsp;
-         <a class="btn btn-sm btn-success" title="Add new" ng-click="show_bank_add()"> Thêm </a>
-         <div class="clearfix"></div>
-       </div>
-       <div class="clearfix"></div>
-    </div>
-    {/literal}
-    <div class="form-group">
-      <div class="col-md-4 align-right">
-      <label class="control-label">Số ngày có lệnh GD</label>
-      </div>
-      <div class="col-md-8">
-        <input ng-model="gd.num_days_gd_pending" ng-trim="true" type="input" name="num_days_gd_pending" class="form-control width100p" 
-         required ng-maxlength="255">
-      </div>
-      <div class="clearfix"></div>
-    </div>
-    <div class="form-group">
-      <div class="col-md-4 align-right">
-      <label class="control-label">Số ngày chờ nhận tiền</label>
-      </div>
-      <div class="col-md-8">
-        <input ng-model="gd.num_days_gd_pending_verification" ng-trim="true" type="input" name="num_days_gd_pending_verification" class="form-control width100p" 
-         required ng-maxlength="255">
-      </div>
-      <div class="clearfix"></div>
-    </div>
-    <div class="form-group">
-      <div class="col-md-4 align-right">
-      <label class="control-label">Số ngày y/c vào approve đã nhận tiền</label>
-      </div>
-      <div class="col-md-8">
-        <input ng-model="gd.num_days_gd_approve" ng-trim="true" type="input" name="num_days_gd_approve" class="form-control width100p" 
-         required ng-maxlength="255">
       </div>
       <div class="clearfix"></div>
     </div>
