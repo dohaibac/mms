de58d0aad98b9e10285e7d92a28c11fd27e489b8
dohaibac <dohaibac@gmail.com>
2016-05-22 11:57:56 +0700

    add form html,css for Plan module

--- front/views/plan/index.php
+++ front/views/plan/index.php
@@ -1,7 +1,3 @@
-<link href="{$app->appConf->theme_default}/css/font-awesome.min.css" rel="stylesheet">
-<link href="{$app->appConf->theme_default}/css/todo_list.css" rel="stylesheet">
-<link href="{$app->appConf->theme_default}/css/sticky.css" rel="stylesheet">
-
 <script type="text/javascript" src="{$app->appConf->theme_default}/js/controllers/plan.js"></script>
 <script type="text/javascript" src="{$app->appConf->theme_default}/js/controllers/group.js"></script>
 <script type="text/javascript" src="{$app->appConf->theme_default}/js/router/group.js"></script>
